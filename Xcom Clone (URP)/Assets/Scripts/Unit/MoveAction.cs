2ba09d20aaf70078ba8a9981c12605081feb7e07
Kshitijv24 <kshitijv24@gmail.com>
2023-03-15 22:46:36 +0530

    Separated the code of Unit Movement from Unit Script to Move Action Script

---
+++ Xcom Clone (URP)/Assets/Scripts/Unit/MoveAction.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveAction : MonoBehaviour
+{
+    [SerializeField] Animator unitAnimator;
+
+    Vector3 targetPosition;
+
+    private void Awake()
+    {
+        targetPosition = transform.position;
+    }
+
+    private void Update()
+    {
+        float stoppingDistance = 0.1f;
+
+        if (Vector3.Distance(transform.position, targetPosition) > stoppingDistance)
+        {
+            Vector3 moveDirection = (targetPosition - transform.position).normalized;
+
+            float moveSpeed = 4f;
+            transform.position += moveDirection * moveSpeed * Time.deltaTime;
+
+            float rotateSpeed = 10f;
+            transform.forward = Vector3.Lerp(transform.forward, moveDirection, rotateSpeed * Time.deltaTime);
+
+            unitAnimator.SetBool("IsWalking", true);
+        }
+        else
+        {
+            unitAnimator.SetBool("IsWalking", false);
+        }
+    }
+
+    public void Move(Vector3 targetPosition)
+    {
+        this.targetPosition = targetPosition;
+    }
+}
--- Xcom Clone (URP)/Assets/Scripts/Unit/Unit.cs
+++ Xcom Clone (URP)/Assets/Scripts/Unit/Unit.cs
@@ -4,14 +4,12 @@ using UnityEngine;
 
 public class Unit : MonoBehaviour
 {
-    [SerializeField] Animator unitAnimator;
-
-    Vector3 targetPosition;
     GridPosition gridPosition;
+    MoveAction moveAction;
 
     private void Awake()
     {
-        targetPosition = transform.position;
+        moveAction = GetComponent<MoveAction>();
     }
 
     private void Start()
@@ -22,25 +20,6 @@ public class Unit : MonoBehaviour
 
     private void Update()
     {
-        float stoppingDistance = 0.1f;
-        
-        if (Vector3.Distance(transform.position, targetPosition) > stoppingDistance)
-        {
-            Vector3 moveDirection = (targetPosition - transform.position).normalized;
-            
-            float moveSpeed = 4f;
-            transform.position += moveDirection * moveSpeed * Time.deltaTime;
-
-            float rotateSpeed = 10f;
-            transform.forward = Vector3.Lerp(transform.forward, moveDirection, rotateSpeed * Time.deltaTime);
-
-            unitAnimator.SetBool("IsWalking", true);
-        }
-        else
-        {
-            unitAnimator.SetBool("IsWalking", false);
-        }
-
         GridPosition newGridPosition = LevelGrid.Instance.GetGridPositoin(transform.position);
 
         if(newGridPosition != gridPosition)
@@ -51,8 +30,5 @@ public class Unit : MonoBehaviour
         }
     }
 
-    public void Move(Vector3 targetPosition)
-    {
-        this.targetPosition = targetPosition;
-    }
+    public MoveAction GetMoveAction() => moveAction;
 }
