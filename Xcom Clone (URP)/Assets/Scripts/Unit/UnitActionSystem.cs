900263820372aab2cfd60c2babe34d7e482241bd
Kshitijv24 <kshitijv24@gmail.com>
2023-03-26 11:15:59 +0530

    Added Action Busy UI
    Which shows busy state and hide all the other actions that the player can take while one action is currently on going.

--- Xcom Clone (URP)/Assets/Scripts/Unit/UnitActionSystem.cs
+++ Xcom Clone (URP)/Assets/Scripts/Unit/UnitActionSystem.cs
@@ -10,6 +10,7 @@ public class UnitActionSystem : MonoBehaviour
 
     public event EventHandler OnSelectedUnitChanged;
     public event EventHandler OnSelectedActionChanged;
+    public event EventHandler<bool> OnBusyChanged;
 
     [SerializeField] Unit selectedUnit;
     [SerializeField] LayerMask unitLayerMask;
@@ -62,11 +63,15 @@ public class UnitActionSystem : MonoBehaviour
     private void SetBusy()
     {
         isBusy = true;
+
+        OnBusyChanged?.Invoke(this, isBusy);
     }
 
     private void ClearBusy()
     {
         isBusy = false;
+
+        OnBusyChanged?.Invoke(this, isBusy);
     }
 
     private bool TryHandleUnitSelection()
