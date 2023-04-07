bcb8cd80145e70e11917c5e565ac9d18e10f7e15
Kshitijv24 <kshitijv24@gmail.com>
2023-04-07 13:45:35 +0530

    Replaced some non-generic code with generic code to reduce the redundancy in the code

--- Xcom Clone (URP)/Assets/Scripts/Unit/Unit.cs
+++ Xcom Clone (URP)/Assets/Scripts/Unit/Unit.cs
@@ -15,18 +15,12 @@ public class Unit : MonoBehaviour
 
     GridPosition gridPosition;
     HealthSystem healthSystem;
-    MoveAction moveAction;
-    SpinAction spinAction;
-    ShootAction shootAction;
     BaseAction[] baseActionArray;
     int actionPoints = ACTION_POINTS_MAX;
 
     private void Awake()
     {
         healthSystem = GetComponent<HealthSystem>();
-        moveAction = GetComponent<MoveAction>();
-        spinAction = GetComponent<SpinAction>();
-        shootAction = GetComponent<ShootAction>();
         baseActionArray = GetComponents<BaseAction>();
     }
 
@@ -55,11 +49,17 @@ public class Unit : MonoBehaviour
         }
     }
 
-    public MoveAction GetMoveAction() => moveAction;
-
-    public SpinAction GetSpinAction() => spinAction;
-
-    public ShootAction GetShootAction() => shootAction;
+    public T GetAction<T>() where T : BaseAction
+    {
+        foreach (BaseAction baseAction in baseActionArray)
+        {
+            if(baseAction is T)
+            {
+                return (T)baseAction;
+            }
+        }
+        return null;
+    }
 
     public GridPosition GetGridPosition() => gridPosition;
 
